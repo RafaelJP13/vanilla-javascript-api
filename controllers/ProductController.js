13746d54af9ff48472b2f4efd9dba69d9ad8135a
Rafael Fernandes <Rafaelsf013@gmail.com>
2021-01-08 09:26:56 -0300

    Criar Product dinamico

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -49,22 +49,39 @@ async function getProduct(req, res, id){
 
 async function createProduct(req, res){
 
-    const product = {
+    try{
+        let body = ''
 
-        title: 'Miband 4',
-        description: `Full colour AMOLED touch display View call, text, and app notifications Activity and swim tracking Water resistant up to 50m*； 3-axis accelerometer, 3-axis gyroscope Up to 20 days battery* A single charge lasts up to 20 days.
-        Instantly view call, text, app notifications and music in play. Keep your hands free as you keep up with life.
-        Running Tracks your pace and step count Maintain a safe workout, whether you re running short or long distance.
-        Swimming 5 ATM water resistant up to 50m Recognizes 5 different swim styles Records 12 data sets including swimming pace and stroke count
-        Sleep monitoring Accurately tracks light and deep sleep as well as heart rate during sleep to help you adjust your sleep patterns.
-        Global Verson`,
-        price: 28.71
+        req.on('data', chunk => {
 
+            body += chunk.toString();
+
+        })
+
+        req.on('end', async () =>{
+
+            const {title, description, price} = JSON.parse(body)
+
+            const product = {
+
+                title,
+                description,
+                price
+
+            }
+
+            const newProduct = await Product.create(product)
+            res.writeHead(201, {'Content-Type':'application/json'})
+            return res.end(JSON.stringify(newProduct))
+
+            
+        })
     }
 
-    const newProduct = await Product.create(product)
-    res.writeHead(201, {'Content-Type':'application/json'})
-    return res.end(JSON.stringify(newProduct))
+    catch(e){
+
+
+    }
 
 }
 
